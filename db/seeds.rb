985ab350fd3a5721d67ec1e57fdaba0839a7ceb7
waynezdev <cas021919@coderacademy.edu.au>
2019-10-29 23:34:13 +1100

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+brands = [
+    { name: "Hilti"},
+    { name: "Makita"},
+    { name: "Bosch"},
+    { name: "Dewalt"},
+    { name: "AEG"},
+    { name: "Milwaukii"},
+    { name: "Panasonic"},
+    { name: "Lucius Fox"},
+]
+
+products = [
+
+    { name: "Miter Saw"},
+    { name: "Table Saw"},
+    { name: "Cordless Drill"},
+    { name: "Reciprocating Saw"},
+    { name: "Circular Saw"},
+    { name: "Jigsaw"},
+    { name: "Random Orbital Sander"},
+    { name: "Paint Sprayer"},
+    { name: "Air Compressor"},
+    { name: "Hammer Drill"},
+    { name: "Wood Router"}
+]
+
+for i in 1..30
+    product = Product.create(
+        price: rand(50000..100000),
+        title: "#{products[:name] Faker::Games::LeagueOfLegends.champion}"
+        brand: 
+        description: Faker::Lorem.paragraph
+    )
