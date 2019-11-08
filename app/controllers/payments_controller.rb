1c46f22b438164c5b9abc5f8d49683ad5a20cc5e
waynezdev <cas021919@coderacademy.edu.au>
2019-11-08 15:43:15 +1100

    updated webhook and user authentication

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,6 @@
 class PaymentsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:webhook]
-    
+    skip_before_action :authenticate_user!, only: [:webhook]
     
     def success
 
